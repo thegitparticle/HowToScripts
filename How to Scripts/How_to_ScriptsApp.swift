ba2f0ce48c204db0e4fa17aedbcd431a24364e3d
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-09-28 01:36:41 +0530

    Initial Commit

---
+++ How to Scripts/How_to_ScriptsApp.swift
@@ -0,0 +1,20 @@
+//
+//  How_to_ScriptsApp.swift
+//  How to Scripts
+//
+//  Created by SAN on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct How_to_ScriptsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
