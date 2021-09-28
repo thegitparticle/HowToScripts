0bb734758bc70f754a130cc8b04f8b0a2185e3f4
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-09-29 00:58:54 +0530

    Get call done - spacecraft api - 2

--- How to Scripts/buckets/getcalls/GetCallsViewModel.swift
+++ How to Scripts/buckets/getcalls/GetCallsViewModel.swift
@@ -7,43 +7,50 @@
 
 import Foundation
 
-class GetCallsViewModel: ObservableObject, Identifiable {
-	
-	init () {
-		getDetailsFromApi()
-	}
-	
-	@Published var listOfCrafts: [GetCallsData] = []
-	
-	private var craftItem: GetCallsData?
-	
-	var id: String {
-		craftItem?.id ?? ""
-	}
-	
-	private func getDetailsFromApi() {
-		guard let url = URL(string: "https://isro.vercel.app/api/spacecrafts") else {
-			return
-		}
-		
-		let request = URLRequest(url: url)
-		
-		URLSession.shared.dataTask(with: request) { data, response, error in
-			
-			if let data = data {
-				if let decodedResponse = try? JSONDecoder().decode(GetCallsResponse.self, from: data) {
-					
-					DispatchQueue.main.async {
-						
-					}
-				}
-			}
-			
-		}.resume()
-		
-		// .resume() is needed for the call to work, else it won't execute
-		// by default url calls happen on background thread, although swiftui can update ui based on background threads functions
-		// it is safe to send the decoded json data using DispatchQueue
-		
-	}
-}
+//class GetCallsViewModel: ObservableObject, Identifiable {
+//
+//	@State private var spaceCraftsHere = [GetCallsData]()
+//
+//	init () {
+//		getDetailsFromApi()
+//	}
+//
+//	@Published var listOfCrafts: [GetCallsData] = []
+//
+//	private var craftItem: GetCallsData?
+//
+//	var id: String {
+//		craftItem?.id ?? ""
+//	}
+//
+//	private func getDetailsFromApi() {
+//
+//		guard let url = URL(string: "https://isro.vercel.app/api/spacecrafts") else {
+//			return
+//		}
+//
+//		let request = URLRequest(url: url)
+//
+//		URLSession.shared.dataTask(with: request) { data, response, error in
+//
+//			if let data = data {
+//				if let decodedResponse = try? JSONDecoder().decode(GetCallsResponse.self, from: data) {
+//
+//					DispatchQueue.main.async {
+//						self.spaceCraftsHere = decodedResponse.spacecrafts
+//					}
+//					print(decodedResponse.spacecrafts)
+//					return
+//				}
+//
+//				print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//			}
+//
+//		}.resume()
+//
+//		// .resume() is needed for the call to work, else it won't execute
+//		// by default url calls happen on background thread, although swiftui can update ui based on background threads functions
+//		// it is safe to send the decoded json data using DispatchQueue
+//
+//	}
+//}
